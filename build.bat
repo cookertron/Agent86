7c6b1f7adb979563f1cd4350acebbbf5f94b906b
cookertron <59415243+cookertron@users.noreply.github.com>
2026-03-12 11:11:37 +0000

    Delete build.bat
    Includes file structure

--- build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "E:\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cl.exe /EHsc /O2 /std:c++17 /Fe:agent86.exe src\main.cpp src\lexer.cpp src\expr.cpp src\symtab.cpp src\encoder.cpp src\asm.cpp src\jit\emitter.cpp src\jit\decoder.cpp src\jit\dos.cpp src\jit\kbd.cpp src\jit\jit.cpp /I src
